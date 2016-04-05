772ffa15be16a2c73bb83c2552b185e910090c52
Moesio Medeiros <moesio@gmail.com>
2016-04-05 16:04:00 -0300

    Incluido validator no controller do Produto. Ainda falta deixar genérico.

--- dgestao/src/main/java/com/seimos/dgestao/controller/ProdutoController.java
+++ dgestao/src/main/java/com/seimos/dgestao/controller/ProdutoController.java
@@ -1,13 +1,17 @@
 package com.seimos.dgestao.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.WebDataBinder;
+import org.springframework.web.bind.annotation.InitBinder;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.seimos.commons.controller.GenericCrudController;
 import com.seimos.commons.service.GenericService;
 import com.seimos.dgestao.domain.Produto;
 import com.seimos.dgestao.service.ProdutoService;
+import com.seimos.dgestao.validator.ProdutoValidator;
 
 /**
  * This controller will response a call about module of Produto type. It's will
@@ -22,6 +26,17 @@ import com.seimos.dgestao.service.ProdutoService;
 public class ProdutoController extends GenericCrudController<Produto> {
 
 	private ProdutoService produtoService;
+	private ProdutoValidator validator;
+	
+	@InitBinder
+	public void initBinder(WebDataBinder binder) {
+		binder.addValidators(validator);
+	}
+
+	@Autowired
+	public void setValidator(ProdutoValidator validator) {
+		this.validator = validator;
+	}
 	
 	@Autowired
 	public void setProdutoService (ProdutoService produtoService) {
